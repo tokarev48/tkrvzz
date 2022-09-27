f4045f795d22ba91efa095d40d8326ca0e0f442d
tokarev48 <113343606+tokarev48@users.noreply.github.com>
2022-09-27 22:21:27 +0300

    Add files via upload

---
+++ ДЗ-1/ex6.py
@@ -0,0 +1,7 @@
+x = 11
+y = 22
+print("До:",x,y)
+c = x
+x = y
+y = c 
+print("После:",x,y)
---
+++ ДЗ-1/ex7.py
@@ -0,0 +1,5 @@
+number = int(input("Введите число:"))
+if number % 2 == 0:
+    print (number, "- четное число")
+else:
+    print (number, "- нечетное число")
