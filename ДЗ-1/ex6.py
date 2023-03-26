97236b3c7bfd78b0ee7f7818b00fec12e76fe00c
tokarev48 <113343606+tokarev48@users.noreply.github.com>
2023-03-26 22:17:32 +0300

    Delete ДЗ-1 directory

--- ДЗ-1/ex6.py
+++
@@ -1,7 +0,0 @@
-x = 11
-y = 22
-print("До:",x,y)
-c = x
-x = y
-y = c 
-print("После:",x,y)
--- ДЗ-1/ex7.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Введите число:"))
-if number % 2 == 0:
-    print (number, "- четное число")
-else:
-    print (number, "- нечетное число")
