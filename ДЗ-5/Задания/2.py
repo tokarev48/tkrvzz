0cc5bf4ccc3dab4f66651483f5bea1e71ac1e1b1
tokarev48 <113343606+tokarev48@users.noreply.github.com>
2023-03-26 22:19:09 +0300

    Delete ДЗ-5 directory

--- ДЗ-5/Задания/2.py
+++
@@ -1,9 +0,0 @@
-def f():
-    N = int(input("Enter integer number: "))
-
-    if (N >= 2):
-        for i in range(2, N+1):
-            if (N % i == 0):
-                print(f"The smallest natural divisor is: {i}")
-                break
-f()
